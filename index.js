14d83a2ec29d9989760d289a65df5cfc968b040b
Nikita Nek Dudnik <nikdudnik@gmail.com>
2019-11-11 00:32:13 +0100

    Setup prettier

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const express = require("express");
+const express = require('express')
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 const state = {
-  loading: true,
-  initialized: false,
-  todos: [],
-  areas: [
-    { title: "Work", id: "0" },
-    { title: "Life", id: "1" }
-  ]
-};
+    loading: true,
+    initialized: false,
+    todos: [],
+    areas: [
+        { title: 'Work', id: '0' },
+        { title: 'Life', id: '1' },
+    ],
+}
 
-const dict = {};
+db.defaults(state).write()
+
+const dict = {}
 const mutate = (id, parser) => {
-  dict[id] = parser;
-};
+    dict[id] = parser
+}
 
-mutate("app/init", (term, params, state) => {
-  return state;
-});
+mutate('app/init', (term, params, state) => {
+    return state
+})
 
-mutate("todo/delete", (term, { todoId }, state) => {
-  const newTodos = [...state.todos.filter(({ id }) => id !== todoId)];
-  state.todos = newTodos;
-  return { todoId };
-});
+mutate('todo/delete', (term, { todoId }, state) => {
+    const newTodos = [...state.todos.filter(({ id }) => id !== todoId)]
+    state.todos = newTodos
+    return { todoId }
+})
 
-mutate("todo/new", (term, { area, text, id }, state) => {
-  const todo = state.todos.find(({ id: todoId }) => id === todoId);
-  state.todos.push((todo && { ...todo, area, text }) || { id, text, area });
-  return { id };
-});
+mutate('todo/new', (term, { area, text, id }, state) => {
+    const todo = state.todos.find(({ id: todoId }) => id === todoId)
+    state.todos.push((todo && { ...todo, area, text }) || { id, text, area })
+    return { id }
+})
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => res.send("Send postage to /api"));
-app.post("/api", (req, res) => {
-  const [tag, params] = req.body;
-  res.send(JSON.stringify((dict[tag] && dict[tag](tag, params, state)) || {}));
-});
+app.get('/', (req, res) => res.send('Send postage to /api'))
+app.post('/api', (req, res) => {
+    const [tag, params] = req.body
+    res.send(JSON.stringify((dict[tag] && dict[tag](tag, params, state)) || {}))
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
